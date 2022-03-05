79b5b80611aab5df68324dd552d09acb3e1b5baa
javohir1077 <hakimovjavohir1077@gmail.com>
2022-03-05 17:27:51 +0500

    qwerty

--- data/config.py
+++ data/config.py
@@ -5,5 +5,5 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+BOT_TOKEN = "5139179189:AAFrHx1I7fJIcDyY9sND4ly_VXVA6qoLreo"
+ADMINS = "1914709503"
